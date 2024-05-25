dfa1bbdcb8dd3d9249c10f1040dbe6d3293debc7
dezeve <dezeve@outlook.com>
2024-05-25 04:54:01 +0300

    feat: identify port service

--- src/utils/scanner.py
+++ src/utils/scanner.py
@@ -6,24 +6,32 @@ def scanPort(selectedTarget, port):
         selectedSocket.settimeout(1)
         result = selectedSocket.connect_ex((selectedTarget, port))
         if result == 0:
-            print(f"Port {port} is open")
+            print(f"{port}\t{identifyPortService(port)}")
     except socket.error as error:
         print(f"Error: Port {port}: {error}")
     finally:
         selectedSocket.close()
 
 def quickScan(selectedTarget):
+    print("Port\tService\n----------------")
     for port in range(1, 1025):
         scanPort(selectedTarget, port)
     print("\nQuick Scan Completed")
 
 def fullScan(selectedTarget):
+    print("Port\tService\n----------------")
     for port in range(1, 65536):
         scanPort(selectedTarget, port)
     print("\nFull Scan Completed")
  
 def customScan(selectedTarget, startPort, endPort):
-	for port in range(startPort, endPort + 1):
-		scanPort(selectedTarget, port)
-	print("\nCustom Scan Completed")
+    print("Port\tService\n----------------")
+    for port in range(startPort, endPort + 1):
+        scanPort(selectedTarget, port)
+    print("\nCustom Scan Completed")
 
+def identifyPortService(port):
+    try: 
+        return socket.getservbyport(port)
+    except:
+        return "Unknown"
